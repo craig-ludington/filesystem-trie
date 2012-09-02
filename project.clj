fe6efb53a917f99b3533f300e94e0fb590ef56e7
Craig Ludington <me@alpheus.me>
2012-09-02 11:17:45 -0500

    Updated URL in project.clj -- no need to update version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject filesystem-trie "0.99.1"
   :description "A filesystem-based trie library."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/craig-ludington/filesystem-trie"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
